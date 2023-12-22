7db70cbf367206d5c9f4848f7de52847be0a13cd
Jenni Delport <jenldelport@gmail.com>
2023-12-22 14:05:16 +0000

    Rework

--- foundation.py
+++ foundation.py
@@ -1,55 +1,91 @@
 print("Welcome to Domestipets Charity Donation page")
 
-canine_list = {"Dry kibble 1kg": 11.95,
-               "Dry kibble 500g": 6.95,
-               "Wet mixed grill can": 6.95,
-               "Shine shampoo": 8.95,
-               "Doggy chews": 4.00}
-
-feline_list = {"Dreamy bites": 3.00,
-               "Wet mixed grill can": 5.95,
-               "Odour spray": 11.00,
-               "Litter fresh": 5.00}
-
-rodent_list = {"Snack seeds": 4.50,
-               "Hay mix": 8.55,
-               "Water bottle": 4.95,
-               "Runner wheel": 10.00}
+donatable_items = {"dry kibble cat": 11.95,
+               "dry kibble dog": 6.95,
+               "wet mixed grill can": 6.95,
+               "shine shampoo": 8.95,
+               "doggy chews": 4.00,
+               "dreamy bites": 3.00,
+               "wet mixed grill can": 5.95,
+               "odour spray": 11.00,
+               "litter fresh": 5.00,
+               "catnip": 4.95,
+               "snack seeds": 4.50,
+               "hay mix": 8.55,
+               "water bottle": 4.95,
+               "runner wheel": 10.00}
 
 shopping_cart = []
 total = 0
 
-#While True:
-type_input = int(input(("Please select pet type from options below:\n1: Canine\n2: Feline"\
-                   "\n3: Rodent\n0: Exit\n")))
+print("----------Donatable items list----------")
+for key, value in donatable_items.items():
+    print(f"{key}: £{value:.2f}")
+print("------------------------------------") 
+
+while True:
+    cart_items = input("Please select item to add to cart (e to exit): ").lower()
+    if cart_items == "e":
+        break
+    elif donatable_items.get(cart_items) is not None:
+        shopping_cart.append(cart_items)
+
+print("----------Your Cart----------")
+for cart_items in shopping_cart:
+    total += donatable_items.get(cart_items)
+print(shopping_cart)
+
+print()
+print(f"Your total is: £{total:.2f}")
+
+added_items = []
+removed_items = []
+
+while True:
+    optional_input = input("Would you like to add or remove items from your cart (e to exit): ").lower()
+    if optional_input == "e":
+        break
+        
+    elif optional_input == "add":
+        add_input = input("Please select item to add to cart (q to quit): ").lower()
+        if add_input == "q":
+            break
+
+        else:
+            shopping_cart.append(added_items)
+
+
+    elif optional_input == "remove":
+         remove_input = input("Please select items to remove (q to quit): ")
+         if remove_input == "q":
+            break
+         
+         else:
+          for i, item in enumerate(shopping_cart, 1):
+            print(i, item)
+            removed_item = input("Please enter the index of the item you would like to remove:\n")
+            index = (index)-1
+            removed_item = shopping_cart[index]
+            shopping_cart.pop(index)
+            print(f'{item} removed!')    
+
+
+    
+
+  
+        
+
+
+
+
+
+#type_input = int(input(("Please select pet type from options below:\n1: Canine\n2: Feline"\
+                  # "\n3: Rodent\n0: Exit\n")))
 # Include some defensive coding for if none of the above are entered
 
-if type_input == 1:
-    print("-------------Canine menu---------------")
-    for key, value in canine_list.items():
-        print(f"{key}: £{value:.2f}")
-    print("----------------------------------------")    
 
-# Insert options to add to cart individual items from canine_menu
-    
-elif type_input == 2:
-    print("-------------Feline menu---------------")
-    for key, value in feline_list.items():
-        print(f"{key}: £{value:.2f}")
-    print("----------------------------------------")   
-
-# Insert options to add to cart individual items from feline_menu    
-    
-elif type_input == 3:
-    print("-------------Rodent menu---------------")
-    for key, value in rodent_list.items():
-        print(f"{key}: £{value:.2f}")
-    print("----------------------------------------") 
-
-# Insert options to add to cart individual items from feline_menu  
-
-else:
-    print("Thank you for viewing our page. We hope you come back soon!")    
+#else:
+    #print("Thank you for viewing our page. We hope you come back soon!")    
 
 # while True:
 #     # Ask user to select an option form the menu
